290142d30bb8e977cc06d94a18bed0e974091d59
Federico Krenn <fedekrenn@gmail.com>
2024-01-29 12:05:40 -0300

    Update favicon and logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom'
+import image from '../assets/logo.webp'
 
 export default function Header () {
   return (
-    <header className='flex justify-around items-center w-2/5 min-h-28 m-auto'>
+    <header className='flex justify-around items-center w-2/5 min-h-32 m-auto'>
       <Link to='/'>
-        <img src='https://i.imgur.com/7I1V7Uu.png' alt='logo' className='w-16 h-16' />
+        <img src={image} alt='logo' className='w-28 h-28 rounded-full' />
       </Link>
       <h2 className='text-4xl font-bold flex-1 text-center'>Sorteo de equipos</h2>
     </header>
